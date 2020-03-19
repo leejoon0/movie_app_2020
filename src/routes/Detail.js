1047eb9a14813a0a60a35c777dc48a20fed96c95
userRND <userRND@idino.co.kr>
2020-03-19 16:44:54 +0900

    보완

---
+++ src/routes/Detail.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Detail.css";
+
+class Detail extends React.Component {
+    componentDidMount() {
+      const { location, history } = this.props;
+      if (location.state === undefined) {
+        history.push("/");
+      }
+    }
+    render() {
+      const { location } = this.props;
+      if (location.state) {
+        return(
+            <div className="detail__container">
+                <img src={location.state.poster} alt={location.state.title} title={location.state.title} />
+                <span>
+                    {location.state.title}
+                </span>
+                <span>
+                    {location.state.summary}
+                </span>
+            </div>
+        );
+      } else {
+        return null;
+      }
+    }
+  }
+  export default Detail;
