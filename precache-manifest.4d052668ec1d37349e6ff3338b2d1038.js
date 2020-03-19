9e9368966c0ebf9c58f0a574789a2c6d8d802a70
userRND <userRND@idino.co.kr>
2020-03-19 14:40:00 +0900

    Updates

---
+++ precache-manifest.4d052668ec1d37349e6ff3338b2d1038.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f424e8ed51a9a584ec9ac3377ccf121c",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "9b78dece9efadf089071",
+    "url": "/movie_app_2020/static/css/main.756e0f15.chunk.css"
+  },
+  {
+    "revision": "5a154eb4d7363b37c6c9",
+    "url": "/movie_app_2020/static/js/2.a079bd8e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app_2020/static/js/2.a079bd8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b78dece9efadf089071",
+    "url": "/movie_app_2020/static/js/main.73dd7a2e.chunk.js"
+  },
+  {
+    "revision": "d5ba99470fbd5d5276a4",
+    "url": "/movie_app_2020/static/js/runtime-main.6a93e83b.js"
+  }
+]);
